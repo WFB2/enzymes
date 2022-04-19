5115e56a96ec0507ea2b52f7ff06223918de7dea
WFB2 <75089017+WFB2@users.noreply.github.com>
2022-04-19 14:03:09 +0200

    clean up

--- 002_merge_all_stations.R
+++ 002_merge_all_stations.R
@@ -15,22 +15,25 @@ enz <- list()
 for (j in 1:length(files)){
   enz[[j]] <- read_excel(files[j], sheet = 1)
 }
-#enz[[2]] <- remove()
 enz <- data.table::rbindlist(enz) %>% as_tibble()
-# add target depth from flowcytometry data
-Flow_depth <- read_excel("../../FlowCyto/Phytoplankton SO287.xlsx",sheet = 2)
 
-Flow_depth <- Flow_depth %>% 
-  select(Station, Target_depth) %>%
-  mutate(depth = case_when(Target_depth  == 6 ~ 1,
-                           Target_depth == 50 ~ 2,
-                           Target_depth > 50 & Target_depth < 200 ~ 3,
-                           Target_depth == 200 ~ 4,
-                           Target_depth > 200 & Target_depth < 1000 ~ 5,
-                           Target_depth == 1000 | Target_depth == 2000 ~ 6,
-                           Target_depth > 2000 ~ 7))
+### add target depth from flowcytometry data
 
-enz$depth <- Flow_depth$depth[match(enz$depth,Flow_depth$Target_depth)]
+# Flow_depth <- read_excel("../../FlowCyto/Phytoplankton SO287.xlsx",sheet = 2)
+# 
+# Flow_depth <- Flow_depth %>% 
+#   select(Station, Target_depth) %>%
+#   mutate(depth = case_when(Target_depth  == 6 ~ 1,
+#                            Target_depth == 50 ~ 2,
+#                            Target_depth > 50 & Target_depth < 200 ~ 3,
+#                            Target_depth == 200 ~ 4,
+#                            Target_depth > 200 & Target_depth < 1000 ~ 5,
+#                            Target_depth == 1000 | Target_depth == 2000 ~ 6,
+#                            Target_depth > 2000 ~ 7))
+
+# enz$depth <- Flow_depth$depth[match(enz$depth,Flow_depth$Target_depth)]
+
+
 write.xlsx2(enz, "../Enzymes_full.xlsx", sheetName = "Sheet1",
             col.names = T, row.names = T, append = F)        
 
